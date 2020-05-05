191e5557712a7111da8f6af834501d03bcbf7a77
alonsoramirezr <a.ramirez.160595@gmail.com>
2020-05-05 19:03:10 -0400

    Se agregan serializadores de la receta

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -44,3 +44,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         # Obtiene las recetas del usuario logeado
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        # Retorna una clase serializer apropiada
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
