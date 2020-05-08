c3e40ef4d1896155a246fa8936f4454853e54a53
alonsoramirezr <a.ramirez.160595@gmail.com>
2020-05-08 16:59:00 -0400

    Se agrega el feature para subir imagenes para las recetas

--- app/core/models.py
+++ app/core/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 
 def recipe_image_file_path(instance, filename):
-    # Genera un path para una nueva imagen de la receta
+    """Generate file path for new recipe image"""
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -76,7 +76,7 @@ class ModelTests(TestCase):
 
     @patch('uuid.uuid4')
     def test_recipe_file_name_uuid(self, mock_uuid):
-        # Prueba de que la imagen se guarda en el lugar correcto
+        """Test that image is saved in the correct location"""
         uuid = 'test-uuid'
         mock_uuid.return_value = uuid
         file_path = models.recipe_image_file_path(None, 'myimage.jpg')
